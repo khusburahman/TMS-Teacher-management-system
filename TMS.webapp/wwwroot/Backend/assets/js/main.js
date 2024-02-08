d6e03c112af7b440700534a09b609047fc24914a
khusburahman <khusburahman49@gmail.com>
2024-02-08 11:42:39 +0600

    add  Dashbord

---
+++ TMS.webapp/wwwroot/Backend/assets/js/main.js
@@ -0,0 +1,29 @@
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
+
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('Active');
+		})
+		li.classList.add('Active');
+	})
+});
+
+
+
+
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+	sidebar.classList.toggle('hide');
+})
+
+
+
+
+
